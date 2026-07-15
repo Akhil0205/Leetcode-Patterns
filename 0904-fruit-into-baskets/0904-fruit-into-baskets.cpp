ebd9f3f11784d59dc8ad9ea0ae181a23e3c3ec24
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-07-16 00:58:09 +0530

    Time: 42 ms (62.36%), Space: 84.9 MB (34.08%) - LeetHub - Time: O(?), Space: O(1)

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,36 +1,88 @@
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
-        
-        unordered_map<int,int> mp;
-
-        int left = 0;
-        int maxi = 0;
-
-        for(int right = 0; right < fruits.size(); right++)
+class Solution{
+    public:
+    int totalFruit(vector<int>& s)
+    {
+        unordered_map<int,int>f;
+        int low=0;
+        int res=0;
+        int n=s.size();
+        for(int high=0;high<n;high++)
         {
-            mp[fruits[right]]++;
+            f[s[high]]++;
 
-            // More than 2 fruit types
-            while(mp.size() > 2)
+            while(f.size()>2)
             {
-                mp[fruits[left]]--;
+                f[s[low]]--;
 
-                if(mp[fruits[left]] == 0)
-                {
-                    mp.erase(fruits[left]);
-                }
+                if(f[s[low]]==0) 
+                f.erase(s[low]);
 
-                left++;
+                low++;
             }
+                int len=high-low+1;
+                res=max(len,res);
 
-            maxi = max(maxi, right - left + 1);
-        }
-
-        return maxi;
+            }
+            
+        
+        return res;
     }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+        
+//         unordered_map<int,int> mp;
+
+//         int left = 0;
+//         int maxi = 0;
+
+//         for(int right = 0; right < fruits.size(); right++)
+//         {
+//             mp[fruits[right]]++;
+
+//             // More than 2 fruit types
+//             while(mp.size() > 2)
+//             {
+//                 mp[fruits[left]]--;
+
+//                 if(mp[fruits[left]] == 0)
+//                 {
+//                     mp.erase(fruits[left]);
+//                 }
+
+//                 left++;
+//             }
+
+//             maxi = max(maxi, right - left + 1);
+//         }
+
+//         return maxi;
+//     }
+// };
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
