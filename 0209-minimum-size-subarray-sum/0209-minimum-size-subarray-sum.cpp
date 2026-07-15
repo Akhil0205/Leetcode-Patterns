ef00d72f7aa13970224932fa178e8e4168b59035
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-07-15 23:21:58 +0530

    Time: 0 ms (100%), Space: 41.8 MB (47.36%) - LeetHub - Time: O(?), Space: O(?)

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,45 +1,56 @@
 class Solution {
 public:
-    int minSubArrayLen(
-        int target,
-        vector<int>& nums
-    ) {
-
-        int left = 0;
-
-        int sum = 0;
-
-        int ans = INT_MAX;
-
-        for(
-            int right=0;
-            right<nums.size();
-            right++
-        )
+    int minSubArrayLen(int target,vector<int>& nums) 
+    {
+        int low=0;
+        int high=0;
+        int sum=0;
+        int res=INT_MAX;
+        int n=nums.size();
+        while(high<n)
         {
-            sum += nums[right];
-
-            while(sum >= target)
+            sum+=nums[high];
+            while(sum>=target)
             {
-                ans =
-                min(
-                ans,
-                right-left+1
-                );
-
-                sum -= nums[left];
-
-                left++;
+                int len=high-low+1;
+                res=min(res,len);
+                sum-=nums[low];
+                low++;
             }
+            high++;
         }
+        if(res==INT_MAX) return 0;
+        return res;
 
-        if(ans == INT_MAX)
-            return 0;
-
-        return ans;
     }
 };
 
+
+// class Solution {
+// public:
+//     int minSubArrayLen(int target,vector<int>& nums
+//     ) {
+
+//         int left = 0;
+//         int sum = 0;
+//         int ans = INT_MAX;
+//         for(int right=0;right<nums.size();right++)
+//         {
+//             sum += nums[right];
+//             while(sum >= target)
+//             {
+//                 ans =min(ans,right-left+1);
+//                 sum -= nums[left];
+//                 left++;
+//             }
+//         }
+//         if(ans == INT_MAX)
+//             return 0;
+
+//         return ans;
+//     }
+// };
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
