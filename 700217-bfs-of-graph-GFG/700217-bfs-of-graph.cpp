bed92b4edd7dc65bcc9d4b00d6944ec5bbb968e8
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-09-12 17:18:31 +0530

    Added solution - Time: O(V + E), Space: O(V)

---
+++ 700217-bfs-of-graph-GFG/700217-bfs-of-graph.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n=adj.size();
+        vector<int>res;
+        vector<bool>vis(n,0);
+        queue<int>q;
+        q.push(0);
+        vis[0]=true;
+        
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            res.push_back(node);
+            for(int i=0;i<adj[node].size();i++)
+            {
+                int neigh=adj[node][i];
+                if(vis[neigh]==false)
+                {
+                    q.push(neigh);
+                    vis[neigh]=true;
+                }
+            }
+        }
+        return res;
+        
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
