460f0d2aa92f0bb08a81a49eb03b7973760ee1f3
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-09-12 19:03:54 +0530

    Time: 27 ms (55.2%), Space: 16.5 MB (67.04%) - LeetHub - Time: O(n*m), Space: O(n*m)

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+    int x[4] = {-1, 1, 0, 0};
+    int y[4] = {0, 0, -1, 1};
+
+    bool isValid(int i, int j, int n, int m)
+    {
+        if(i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+
+        return true;
+    }
+
+    void dfs(vector<vector<char>>& a, int n, int m, int i, int j,
+             vector<vector<bool>>& vis)
+    {
+        vis[i][j] = 1;
+
+        for(int k = 0; k < 4; k++)
+        {
+            int row = i + x[k];
+            int col = j + y[k];
+
+            if(isValid(row, col, n, m) &&
+               a[row][col] == '1' &&
+               vis[row][col] == 0)
+            {
+                dfs(a, n, m, row, col, vis);
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int res = 0;
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j] == '1' && vis[i][j] == 0)
+                {
+                    dfs(grid, n, m, i, j, vis);
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
