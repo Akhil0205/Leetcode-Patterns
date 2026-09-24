64cdac65f6ddec7797d642692acb310af8bafa93
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-09-24 14:15:55 +0530

    Time: 746 ms (15.03%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1148-recyclable-and-low-fat-products/1148-recyclable-and-low-fat-products.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select p.product_id 
+from Products p
+where low_fats = 'Y'
+and recyclable ='Y';
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
