5fb05f2298195e54e6dd078d97e157c095c00839
Akhil Mohan <149316041+Akhil0205@users.noreply.github.com>
2026-09-24 14:04:20 +0530

    Time: 1033 ms (66.03%), Space: 0B (100%) - LeetHub - Time: O(N log N + M), Space: O(N)

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,13 @@
+SELECT 
+    r.contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
